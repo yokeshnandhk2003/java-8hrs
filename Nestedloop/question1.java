b2db36d31873df076858aa5b8ff35b6257cf58b5
yokeshnandhk2003 <147370191+yokeshnandhk2003@users.noreply.github.com>
2025-06-21 13:26:05 +0530

    Add files via upload

---
+++ Nestedloop/question1.java
@@ -0,0 +1,19 @@
+package Nestedloop;
+//print this output
+//***
+//***
+//***
+public class question1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<=3;j++) {
+				System.out.print("*");	
+			}
+			System.out.println();	
+			}
+		
+
+	}
+
+}
---
+++ Nestedloop/question2.java
@@ -0,0 +1,19 @@
+package Nestedloop;
+//print this output
+//*
+//**
+//***
+public class question2 {
+
+	public static void main(String[] args) {
+		for(int j=1;j<=3;j++) {
+			for(int i=1;i<=j;i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
